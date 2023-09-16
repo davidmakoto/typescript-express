44cbc6e8a087059f933240242f4282f69cd1c818
David <davidmacotoward@gmail.com>
2023-09-16 16:33:26 -0700

    refactored routes to routes folder and homeRoute path

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,10 @@
 import express, { Application, Request, Response } from 'express';
-
+import { homeRoute } from './routes/homeRoute';
 const app: Application = express();
 
 const PORT: number = 3001;
 
-app.use('/', (req: Request, res: Response): void => {
-    res.send('Hello world!');
-});
+app.use('/', homeRoute);
 
 app.listen(PORT, (): void => {
     console.log('SERVER IS UP ON PORT:', PORT);
